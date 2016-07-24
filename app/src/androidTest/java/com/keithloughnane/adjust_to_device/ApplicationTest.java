56497dc8d95ee8f40aec9ecce1a97382a0cf6c4a
Keith Loughnane <keith.loughnane@gmail.com>
2016-07-24 21:20:37 +0300

    First Commit

---
+++ app/src/androidTest/java/com/keithloughnane/adjust_to_device/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.keithloughnane.adjust_to_device;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application>
+{
+	public ApplicationTest()
+	{
+		super(Application.class);
+	}
+}
